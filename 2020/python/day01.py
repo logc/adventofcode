f3005d960940df8d5e112e3e9c91749e52a2cc4a
Luis Osa <luis.osa.gdc@gmail.com>
2020-12-01 21:56:29 +0100

    Refactor Python day1

--- 2020/python/day01.py
+++ 2020/python/day01.py
@@ -1,25 +1,24 @@
-import sys
+def solve_one(numbers):
+    for m in numbers:
+        for n in numbers:
+            if m + n == 2020:
+                return m * n
 
-with open("day01.txt", "r") as infile:
-    numbers = [int(line.strip()) for line in infile]
 
-finished = False
-for m in numbers:
-    for n in numbers:
-        if m + n == 2020:
-            print(m * n)
-            finished = True
-    if finished:
-        break
+def solve_two(numbers):
+    for m in numbers:
+        for n in numbers:
+            for i in numbers:
+                if m + n + i == 2020:
+                    return m * n * i
 
-found = False
-for m in numbers:
-    for n in numbers:
-        for i in numbers:
-            if m + n + i == 2020:
-                print(m * n * i)
-                finished = True
-        if found:
-            break
-    if found:
-        break
+
+if __name__ == '__main__':
+    with open("day01.txt", "r") as infile:
+        puzzle_input = [int(line.strip()) for line in infile]
+    one = solve_one(puzzle_input)
+    two = solve_two(puzzle_input)
+    assert one == 445536
+    assert two == 138688160
+    print("Puzzle #1: {}".format(solve_one(puzzle_input)))
+    print("Puzzle #2: {}".format(solve_two(puzzle_input)))
