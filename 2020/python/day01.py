bb19293fd76aca6ced73cbe467bb321d504a1163
Luis Osa <luis.osa.gdc@gmail.com>
2020-12-01 22:11:37 +0100

    Solve with less complexity

--- 2020/python/day01.py
+++ 2020/python/day01.py
@@ -1,24 +1,26 @@
-def solve_one(numbers):
-    for m in numbers:
-        for n in numbers:
-            if m + n == 2020:
-                return m * n
+def solve_one(numbers, total):
+    seen = set()
+    for n in numbers:
+        diff = total - n
+        if diff in seen:
+            return diff * n
+        else:
+            seen.add(n)
 
 
 def solve_two(numbers):
-    for m in numbers:
-        for n in numbers:
-            for i in numbers:
-                if m + n + i == 2020:
-                    return m * n * i
+    for pos, n in enumerate(numbers):
+        rest = numbers[pos:]
+        total = 2020 - n
+        partial = solve_one(rest, total)
+        if partial is not None:
+            return partial * n
 
 
 if __name__ == '__main__':
     with open("day01.txt", "r") as infile:
         puzzle_input = [int(line.strip()) for line in infile]
-    one = solve_one(puzzle_input)
+    one = solve_one(puzzle_input, 2020)
     two = solve_two(puzzle_input)
-    assert one == 445536
-    assert two == 138688160
-    print("Puzzle #1: {}".format(solve_one(puzzle_input)))
-    print("Puzzle #2: {}".format(solve_two(puzzle_input)))
+    print("Puzzle #1: {}".format(one))
+    print("Puzzle #2: {}".format(two))
